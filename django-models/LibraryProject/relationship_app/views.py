0d028fbc6b4db8dd1d91264e9f7d29e39211c01f
eMacharia001 <machariasensei@gmail.com>
2025-11-18 08:19:56 +0300

    Add views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book, Library, Author
 
 # Function-Based View
 def list_all_books(request):
