3500b1a62a410b2595fbbed85a9bd01b36a6191a
eMacharia001 <machariasensei@gmail.com>
2025-11-18 06:48:39 +0300

    Add FBV and CBV scripts in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
-# Function-based view to list all books
-def book_list(request):
-    books = Book.objects.all()  # Fetch all books from the database
-    return render(request, 'relationship_app/book_list.html', {'books': books})
+# Function-Based View
+def list_all_books(request):
+    books = Book.objects.all()
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return render(request, "relationship_app/book_list.html", {"books_text": output})
+
+# Class-Based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
